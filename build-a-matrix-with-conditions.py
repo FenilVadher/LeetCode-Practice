82003ca44382ce8d9aedf20948edfce92d8231c4
Fenil Vadher <fenilvadher0503@gmail.com>
2024-07-21 19:17:27 +0530

    bulid matrix with condition file added

--- build-a-matrix-with-conditions.py
+++ build-a-matrix-with-conditions.py
@@ -1,3 +1,20 @@
+# Input: k = 3, rowConditions = [[1,2],[3,2]], colConditions = [[2,1],[3,2]]
+# Output: [[3,0,0],[0,0,1],[0,2,0]]
+# Explanation: The diagram above shows a valid example of a matrix that satisfies all the conditions.
+# The row conditions are the following:
+# - Number 1 is in row 1, and number 2 is in row 2, so 1 is above 2 in the matrix.
+# - Number 3 is in row 0, and number 2 is in row 2, so 3 is above 2 in the matrix.
+# The column conditions are the following:
+# - Number 2 is in column 1, and number 1 is in column 2, so 2 is left of 1 in the matrix.
+# - Number 3 is in column 0, and number 2 is in column 1, so 3 is left of 2 in the matrix.
+# Note that there may be multiple correct answers.
+# Example 2:
+
+# Input: k = 3, rowConditions = [[1,2],[2,3],[3,1],[2,3]], colConditions = [[2,1]]
+# Output: []
+# Explanation: From the first two conditions, 3 has to be below 1 but the third conditions needs 3 to be above 1 to be satisfied.
+# No matrix can satisfy all the conditions, so we return the empty matrix.
+
 from collections import defaultdict
 from typing import List
 
